ebd46f5cef7e84839304821757b559f1937aeff3
יובל קוג'מן (Yuval Kogman) <nothingmuch@woobling.org>
2006-08-10 15:10:58 +0000

    Import of NUFFIN/Catalyst-Plugin-Session-DynamicExpiry-0.02 from CPAN.
    gitpan-cpan-distribution: Catalyst-Plugin-Session-DynamicExpiry
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         NUFFIN/Catalyst-Plugin-Session-DynamicExpiry-0.02.tar.gz
    gitpan-cpan-author:       NUFFIN
    gitpan-cpan-maturity:     released

--- lib/Catalyst/Plugin/Session/DynamicExpiry.pm
+++ lib/Catalyst/Plugin/Session/DynamicExpiry.pm
@@ -1,46 +1,100 @@
 package Catalyst::Plugin::Session::DynamicExpiry;
+use base qw/Class::Accessor::Fast/;
 
 use NEXT;
 
-our $VERSION='0.01';
+our $VERSION='0.02';
 
-use base qw/Catalyst::Plugin::Session::State/;
+__PACKAGE__->mk_accessors(qw/_session_time_to_live/);
 
-sub calc_cookie_expiry {
-    my $c=shift;
-    if( $c->session->{__time_to_live} ) {
-	return time() + $c->session->{__time_to_live};
+sub session_time_to_live {
+    my ( $c, @args ) = @_;
+
+    if ( @args ) {
+        $c->_session_time_to_live($args[0]);
+        eval { $c->_session->{__time_to_live} = $args[0] };
     }
-    else {
-	return $c->NEXT::calc_cookie_expiry(@_);
+
+    return $c->_session_time_to_live || eval { $c->_session->{__time_to_live} };
+}
+
+sub calculate_initial_session_expires {
+    my $c = shift;
+    
+    if ( defined( my $ttl = $c->_session_time_to_live ) ) {
+        $c->log->debug("Overridden time to live: $ttl") if $c->debug;
+        return time() + $ttl;
     }
+
+    return $c->NEXT::calculate_initial_session_expires( @_ );
+}
+
+sub calculate_extended_session_expires {
+    my $c = shift;
+
+
+    if ( defined(my $ttl = $c->session_time_to_live) ) {
+        $c->log->debug("Overridden time to live: $ttl") if $c->debug;
+        return time() + $ttl;
+    }
+
+    return $c->NEXT::calculate_extended_session_expires( @_ );
+}
+
+sub _save_session {
+    my $c = shift;
+
+    if ( my $session_data = $c->_session ) {
+        if ( defined( my $ttl = $c->_session_time_to_live ) ) {
+            $session_data->{__time_to_live} = $ttl;
+        }
+    }
+
+    $c->NEXT::_save_session( @_ );
 }
 
 1;
 
 =head1 NAME
 
-Catalyst::Plugin::Session::DynamicExpiry - set expiry of cookies per session
+Catalyst::Plugin::Session::DynamicExpiry - per-session custom expiry times
 
 =head1 SYNOPSIS
 
     # put Session::DynamicExpiry in your use Catalyst line
     
     if ($c->req->param('remember') { 
-      $c->session->{__time_to_live}=604800 # expire in one week.
+      $c->session_time_to_live( 604800 ) # expire in one week.
     }
 
 =head1 DESCRIPTION
 
-This module allows you to expire session cookies indvidually per session. If loaded,
-it looks for a C<__cookie_time_to_live> key in the session hash, and sets expiry to that 
-many seconds into the future. Note that the session cookie is set on every request, 
-so a expiry of one week will stay as long as the user visits the site at least once
-a week.
+This module allows you to expire session cookies indvidually per session.
+
+If the C<session_time_to_live> field is defined it will set expiry to that many
+seconds into the future. Note that the session cookie is set on every request,
+so a expiry of one week will stay as long as the user visits the site at least
+once a week.
+
+Once ttl has been set for a session the ttl will be stored in the
+C<__time_to_live> key within the session data itself, and reused for subsequent
+request, so you only need to set this once per session (not once per request).
+
+This is unlike the ttl option in the config in that it allows different
+sessions to have different times, to implement features like "remember me"
+checkboxes.
+
+=head1 METHODS
+
+=head2 session_time_to_live $ttl
+
+To set the TTL for this session use this method.
 
 =head1 OVERRIDDEN METHODS
 
-=head2 calc_cookie_expiry
+=head2 calculate_initial_session_expires
+
+=head2 calculate_extended_session_expires
 
 Overridden to implement dynamic expiry functionality.
 
@@ -53,6 +107,7 @@ Overridden to implement dynamic expiry functionality.
 =head1 AUTHOR
 
 Marcus Ramberg, C<mramberg@cpan.org>
+Yuval Kogman
 
 =head1 LICENSE
 
